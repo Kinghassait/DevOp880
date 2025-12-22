700048dca1f8974a19dd26ad91635298625592fa
Phanupong_Dex <phanupong.ka@ku.th>
2025-12-22 14:51:13 +0700

    errortest

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -17,7 +17,6 @@ describe('Calculator', () => {
         expect(divide(10, 2)).toBe(5);
     });
 
-     test('adds -2 + -3 to equal 5', () => {
-        expect(add(2, 3)).toBe(5);
+     test('adds -2 + -3 to equal
     });
 });
